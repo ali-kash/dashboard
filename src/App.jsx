a7d472115093d1b539d42b78283eb73af71344ee
Ali <ali.rf.kashani@gmail.com>
2024-03-29 13:48:49 -0400

    create files for mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,68 @@
-import {
-	createBrowserRouter,
-	RouterProvider,
-	Route,
-	Link,
-} from 'react-router-dom'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
 import Home from './pages/home/Home'
 import Users from './pages/users/Users'
 import Products from './pages/products/Products'
+import { Login } from './pages/login/Login'
+
+import { Topbar } from './components/topbar/Topbar'
+import { Sidebar } from './components/sidebar/Sidebar'
+import { Footer } from './components/footer/Footer'
 
 import './styles/global.scss'
 
 function App() {
+	const [theme, colorMode] = useMode()
+
+	const Layout = () => {
+		return (
+			<ColorModeContext.Provider value={colorMode}>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<div className='app'>
+						<main className='content'>
+							<Topbar />
+							<div className='container'>
+								<div className='menuContainer'>
+									<Sidebar />
+								</div>
+								<div className='contentContainer'>
+									<Outlet />
+								</div>
+							</div>
+							<Footer />
+						</main>
+					</div>
+				</ThemeProvider>
+			</ColorModeContext.Provider>
+		)
+	}
+
 	const router = createBrowserRouter([
 		{
 			path: '/',
-			element: <Home />,
+			element: <Layout />,
+			children: [
+				{
+					path: '/',
+					element: <Home />,
+				},
+				{
+					path: '/users',
+					element: <Users />,
+				},
+				{
+					path: '/products',
+					element: <Products />,
+				},
+			],
 		},
 		{
-			path: 'users',
-			element: <Users />,
-		},
-		{
-			path: 'products',
-			element: <Products />,
+			path: '/login',
+			element: <Login />,
 		},
 	])
 
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,5 @@
+import './topbar.scss'
+
+export const Topbar = () => {
+	return <div className='topbar'>Topbar</div>
+}
