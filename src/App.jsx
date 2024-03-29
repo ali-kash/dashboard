b545886a2717ded27a0f827caeac58aaa920e4b0
Ali <ali.rf.kashani@gmail.com>
2024-03-29 16:30:33 -0400

    sidebar navigation - need to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,57 @@
+import { Routes, Route } from 'react-router-dom'
+
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
-
-import Home from './pages/home/Home'
-import Users from './pages/users/Users'
-import Products from './pages/products/Products'
-import { Login } from './pages/login/Login'
-
+// GLOBAL
 import { Topbar } from './components/global/Topbar'
-import { Sidebar } from './components/global/Sidebar'
+import { ProSidebar } from './components/global/ProSidebar'
 import { Footer } from './components/global/Footer'
+
+import Dashboard from './scenes/dashboard'
+// import Team from './scenes/team'
+// import Invoices from './scenes/invoices'
+// import Contacts from './scenes/contacts'
+// import Bar from './scenes/bar'
+// import Form from './scenes/form'
+// import Line from './scenes/line'
+// import Pie from './scenes/pie'
+// import FAQ from './scenes/faq'
+// import Calendar from './scenes/calendar'
+// import Geography from './scenes/geography'
+// import Login from './scenes/login'
 
 import './styles/global.scss'
 
 function App() {
 	const [theme, colorMode] = useMode()
 
-	const Layout = () => {
-		return (
-			<ColorModeContext.Provider value={colorMode}>
-				<ThemeProvider theme={theme}>
-					<CssBaseline />
-					<div className='app'>
-						<main className='content'>
-							<Topbar />
-							<div className='container'>
-								<div className='menuContainer'>
-									<Sidebar />
-								</div>
-								<div className='contentContainer'>
-									<Outlet />
-								</div>
-							</div>
-							<Footer />
-						</main>
-					</div>
-				</ThemeProvider>
-			</ColorModeContext.Provider>
-		)
-	}
-
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <Layout />,
-			children: [
-				{
-					path: '/',
-					element: <Home />,
-				},
-				{
-					path: '/users',
-					element: <Users />,
-				},
-				{
-					path: '/products',
-					element: <Products />,
-				},
-			],
-		},
-		{
-			path: '/login',
-			element: <Login />,
-		},
-	])
-
-	return <RouterProvider router={router} />
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='app'>
+					<ProSidebar />
+					<main className='content'>
+						<Topbar />
+						<Routes>
+							<Route path='/' element={<Dashboard />} />
+							{/* <Route path='/team' element={<Team />} /> */}
+							{/* <Route path='/contacts' element={<Contacts />} /> */}
+							{/* <Route path='/invoices' element={<Invoices />} /> */}
+							{/* <Route path='/form' element={<Form />} /> */}
+							{/* <Route path='/bar' element={<Bar />} /> */}
+							{/* <Route path='/pie' element={<Pie />} /> */}
+							{/* <Route path='/line' element={<Line />} /> */}
+							{/* <Route path='/faq' element={<FAQ />} /> */}
+							{/* <Route path='/calendar' element={<Calendar />} /> */}
+							{/* <Route path='/geography' element={<Geography />} /> */}
+						</Routes>
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	)
 }
 
 export default App
--- src/pages/users/Users.jsx
+++
@@ -1,7 +0,0 @@
-import './users.scss'
-
-const Users = () => {
-	return <div className='user'>Users</div>
-}
-
-export default Users
